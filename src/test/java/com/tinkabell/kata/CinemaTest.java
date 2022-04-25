56187b44d190e92e7eccb1c3b3ae4273798bdde1
Mark Ingamells <mark.ingamells@gmail.com>
2022-04-25 17:24:20 +0100

    RED test correct seat names for Cinnamon Cinema Kata

--- src/test/java/com/tinkabell/kata/CinemaTest.java
+++ src/test/java/com/tinkabell/kata/CinemaTest.java
@@ -23,9 +23,36 @@ class CinemaTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1: A1", "2: A1, A2", "3: A1, A2, A3"}, delimiter = ':')
+    @CsvSource(value = {
+            "1: A1",
+            "2: A1, A2",
+            "3: A1, A2, A3"
+    }, delimiter = ':')
     void checkAllocateSeats(int n, String expected) {
-        assertEquals(expected, cinnamon.allocateSeats(n), "Can allocate " + n + " seat");
+        assertEquals(expected, cinnamon.allocateSeats(n), "Can allocate " + n + " seats");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1: 1: A1",
+            "1: 5: A5",
+            "1: 6: B1",
+            "1: 8: B3",
+            "1: 15: C5",
+            "2: 1: A1, A2",
+            "2: 2: A3, A4",
+            "2: 3: A5, B1",
+            "2: 7: C3, C4",
+            "3: 1: A1, A2, A3",
+            "3: 2: A4, A5, B1",
+            "3: 4: B5, C1, C2",
+            "3: 5: C3, C4, C5"
+    }, delimiter = ':')
+    void checkSeatNames(int request, int n, String expected) {
+        for (int i = 0; i < n-1; i++) {
+            cinnamon.allocateSeats(request);
+        }
+        assertEquals(expected, cinnamon.allocateSeats(request), "Correctly allocate " + request + " seats");
     }
 
     @Test
